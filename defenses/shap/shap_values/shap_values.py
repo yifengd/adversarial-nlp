4db5b6638cae7c8a1bae606c440c6685e695f34b
karl-richter <karl.richter@tum.de>
2022-06-11 08:22:41 +0200

    experiment: shap exact

--- defenses/shap/shap_values/shap_values.py
+++ defenses/shap/shap_values/shap_values.py
@@ -74,7 +74,7 @@ def main(params: dict):
         model=predict_labels,
         masker=tokenizer,
         link=shap.links.logit,
-        algorithm="additive",
+        algorithm="exact",
     )
 
     # calculate the SHAP values
